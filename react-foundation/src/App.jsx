105850f8d3be89ae4d4734600ee14f73e21c757b
Darryl Le Roux <devdarryl@outlook.com>
2022-11-26 13:47:43 +0200

    Added conditionals

--- react-foundation/src/App.jsx
+++ react-foundation/src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 import "./App.css";
 
 function App() {
+  const [showEvents, setShowEvents] = useState(true);
   const [events, setEvents] = useState([
     { title: "Marios birthday bash", id: 1 },
     { title: "Bowsers lives stream", id: 2 },
@@ -9,32 +10,57 @@ function App() {
   ]);
 
   const handleClick = (id) => {
-    setEvents(
+    // If you need to use the previous state, always use the prev state inside a callback function.
+    setEvents((prevEvents) => {
       // If the id matches the event.id, then remove it from the array
       // Return the new array where the id does not match the event.id (Ones that are not clicked)
-      events.filter((event) => {
+      return prevEvents.filter((event) => {
         return id !== event.id;
-      })
-    );
+      });
+    });
     console.log(id);
   };
 
   return (
     <div className="App">
-      {events.map((event) => {
-        return (
-          <div key={event.id}>
-            <h2>{event.title}</h2>
-            <button
-              onClick={() => {
-                handleClick(event.id);
-              }}
-            >
-              Delete Event
-            </button>
-          </div>
-        );
-      })}
+      {showEvents && (
+        <div>
+          <button
+            onClick={() => {
+              setShowEvents(false);
+            }}
+          >
+            Hide Events
+          </button>
+        </div>
+      )}
+      {!showEvents && (
+        <div>
+          <button
+            onClick={() => {
+              setShowEvents(true);
+            }}
+          >
+            Show Events
+          </button>
+        </div>
+      )}
+      {/* use showEvents as set as true, and if not, then run the code at the right of the && */}
+      {showEvents &&
+        events.map((event) => {
+          return (
+            <div key={event.id}>
+              <h2>{event.title}</h2>
+              <button
+                onClick={() => {
+                  handleClick(event.id);
+                }}
+              >
+                Delete Event
+              </button>
+            </div>
+          );
+        })}
     </div>
   );
 }
