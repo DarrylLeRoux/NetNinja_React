8b26b0bfdf9b85f49e3cc0c971434c40711c42fd
Darryl Le Roux <devdarryl@outlook.com>
2022-11-26 13:28:12 +0200

    Remove a item by id

--- react-foundation/src/App.jsx
+++ react-foundation/src/App.jsx
@@ -2,25 +2,36 @@ import { useState } from "react";
 import "./App.css";
 
 function App() {
-  const [name, setName] = useState("Mario");
   const [events, setEvents] = useState([
     { title: "Marios birthday bash", id: 1 },
     { title: "Bowsers lives stream", id: 2 },
     { title: "Race on Moo Moo farm", id: 3 },
   ]);
 
-  const handleClick = () => {
-    setName("Darryl");
+  const handleClick = (id) => {
+    setEvents(
+      // If the id matches the event.id, then remove it from the array
+      // Return the new array where the id does not match the event.id (Ones that are not clicked)
+      events.filter((event) => {
+        return id !== event.id;
+      })
+    );
+    console.log(id);
   };
 
   return (
     <div className="App">
-      <h1>My name is {name}</h1>
-      <button onClick={handleClick}>Change Name</button>
       {events.map((event) => {
         return (
           <div key={event.id}>
             <h2>{event.title}</h2>
+            <button
+              onClick={() => {
+                handleClick(event.id);
+              }}
+            >
+              Delete Event
+            </button>
           </div>
         );
       })}
