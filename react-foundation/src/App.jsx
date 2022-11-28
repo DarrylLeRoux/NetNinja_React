bb0faf09322c147ab7766ba944cefdf9f65603b9
Darryl Le Roux <devdarryl@outlook.com>
2022-11-28 22:23:23 +0200

    Added props to the title component

--- react-foundation/src/App.jsx
+++ react-foundation/src/App.jsx
@@ -22,9 +22,11 @@ function App() {
     console.log(id);
   };
 
+  const subtitle = "All the latest events in Marioland";
+
   return (
     <div className="App">
-      <Title />
+      <Title title="Events in Your Area" subtitle={subtitle} />
       {showEvents && (
         <div>
           <button
