8f40d9807a6840c919ec9cf1f8a32c4e92014ee5
Darryl Le Roux <devdarryl@outlook.com>
2022-11-29 21:06:12 +0200

    Implemented createPortal

--- react-foundation/src/components/Modal.jsx
+++ react-foundation/src/components/Modal.jsx
@@ -1,11 +1,15 @@
 import "./modal.css";
+import { createPortal } from "react-dom";
 
 const Modal = ({ children, handleClose }) => {
-  return (
+  return createPortal(
     <div className="modal-backdrop">
-      <div className="modal">{children}</div>
-      <button onClick={handleClose}>close</button>
-    </div>
+      <div className="modal">
+        {children}
+        <button onClick={handleClose}>close</button>
+      </div>
+    </div>,
+    document.body
   );
 };
 
