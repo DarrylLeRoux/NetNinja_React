e3d92b3fcb1b519c81f7e9786638398584598f81
Darryl Le Roux <devdarryl@outlook.com>
2022-12-07 22:43:56 +0200

    Added input capture and date capture

--- react-foundation/src/components/NewEventForm.jsx
+++ react-foundation/src/components/NewEventForm.jsx
@@ -1,21 +1,30 @@
+import { useState } from "react";
 import "./NewEventForm.css";
 
 import React from "react";
 
 function NewEventForm() {
+  const [title, setTitle] = useState("");
+  const [date, setDate] = useState("");
+
+  // const handleChange = (e) => {
+  //   setTitle(e.target.value);
+  // };
   return (
     <form className="new-event-form">
       <label>
         <span>Event Title</span>
-        <input type="text" />
+        <input type="text" onChange={(e) => setTitle(e.target.value)} />
       </label>
 
       <label>
         <span>Event Date:</span>
-        <input type="date" />
+        <input type="date" onChange={(e) => setDate(e.target.value)} />
       </label>
 
       <button type="submit">Submit</button>
+      <p>tite - {title}</p>
+      <p>date - {date}</p>
     </form>
   );
 }
