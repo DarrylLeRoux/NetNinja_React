19fd9df13af1baf036473634f3b945b443949048
Darryl Le Roux <devdarryl@outlook.com>
2022-12-13 22:47:57 +0200

    Added styles for the tripList

--- tripago/src/components/TripList.js
+++ tripago/src/components/TripList.js
@@ -1,13 +1,30 @@
-import React from "react";
+import { useState, useEffect } from "react";
+
+import "./tripList.css";
 
 const TripList = () => {
-  fetch("http://localhost:3000/trips")
-    .then((response) => response.json())
-    .then((json) => console.log(json));
+  const [trips, setTrips] = useState([]);
 
+  useEffect(() => {
+    fetch("http://localhost:3000/trips")
+      .then((response) => response.json())
+      .then((json) => setTrips(json));
+  }, []);
+
+  console.log(trips);
   return (
-    <div>
+    <div className="trip-list">
       <h2>Trip List</h2>
+      <ul>
+        {trips.map((trip) => {
+          return (
+            <li key={trip.id}>
+              <h3>{trip.title}</h3>
+              <p>{trip.price}</p>
+            </li>
+          );
+        })}
+      </ul>
     </div>
   );
 };
